e1245be498d187e37d0b706c65848ef530545fdd
Nazarali Serikbay <serikbaynazarali@gmail.com>
2025-02-12 21:18:29 +0500

    application app complete
    page: index, delivery and payment, contact, about done

--- app/main/urls.py
+++ app/main/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('delivery-and-payment/', views.delivery_pay, name='delivery and payment'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
 ]
 
