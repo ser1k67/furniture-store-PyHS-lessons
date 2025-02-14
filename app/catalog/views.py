61b3cb050b336d4c563b6cd374ce451b85e81d20
Nazarali Serikbay <serikbaynazarali@gmail.com>
2025-02-14 12:33:20 +0500

    added goods app, connected catalog & product html

---
+++ app/catalog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def catalog(request):
+    return render(request, 'catalog/catalog.html')
+
+def product(request):
+    return render(request, 'catalog/product.html')
