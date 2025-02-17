52e5f7fe3533864a51fc3c711167bba22727ab31
Nazarali Serikbay <serikbaynazarali@gmail.com>
2025-02-17 16:22:31 +0500

    adding fixtures for goods, upgrade admin page, and another changes

--- app/catalog/apps.py
+++ app/catalog/apps.py
@@ -1,6 +1,8 @@
+from tabnanny import verbose
 from django.apps import AppConfig
 
 
 class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'catalog'
+    verbose_name = 'товары' #http://127.0.0.1:8000/admin/ 
