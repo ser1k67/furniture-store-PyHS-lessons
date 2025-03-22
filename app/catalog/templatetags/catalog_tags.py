b6c7ef5620b98e384f977dda08004af8e77ff28a
Nazarali Serikbay <serikbaynazarali@gmail.com>
2025-03-22 16:44:40 +0500

    personal tag for add request params to current request params

--- app/catalog/templatetags/catalog_tags.py
+++ app/catalog/templatetags/catalog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.http import urlencode
 
 from catalog.models import Categories
 
@@ -8,3 +9,10 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+# добавляет к существующим параметрам новые параметры запроса
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
