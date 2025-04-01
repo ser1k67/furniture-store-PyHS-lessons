65564af657b1d6204ac0b69a592ada1e613a5181
Nazarali Serikbay <serikbaynazarali@gmail.com>
2025-04-02 01:31:51 +0500

    simple search

--- catalog/views.py
+++ catalog/views.py
@@ -3,17 +3,21 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 
 from catalog.models import Categories, Products
+from catalog.utils import q_search
 
 # Create your views here.
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     # параметры get 
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    q = request.GET.get('q', None)
 
     # отображение карточек товаров по категориям
     if category_slug == "vse-tovary":
         goods = Products.objects.all()
+    elif q: # поиск
+        goods = q_search(q)
     else:
         categories  = Categories.objects.get(slug=category_slug)
         goods = get_list_or_404(Products.objects.filter(category_id=categories.id))
