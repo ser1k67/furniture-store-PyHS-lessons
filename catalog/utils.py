02813e88fca2f1bc991f157f3eef433119522a9b
Nazarali Serikbay <serikbaynazarali@gmail.com>
2025-04-11 15:49:03 +0500

    search 2.0

--- catalog/utils.py
+++ catalog/utils.py
@@ -6,3 +6,13 @@ from catalog.models import Products
 def q_search(q):
     if q.isdigit() and len(q) <= 5:
         return Products.objects.filter(id=int(q))
+    
+    words = [word for word in q.split() if len(word) > 2]
+
+    q_objects = Q()
+
+    for word in words:
+        q_objects |= Q(name__icontains=word)
+        q_objects |= Q(description__icontains=word)
+    
+    return Products.objects.filter(q_objects)
